de56f0c44d5d9f7cb5ea932ad723cf896496ac64
Max Portador <deus7@protonmail.com>
2021-11-13 12:42:17 +0300

    fix [task 2] теперь проходит с 0

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -17,8 +17,8 @@
 
 import {
     __, allPass, both, compose, set, gt, lt, pipe,
-    tap, length, test, ifElse, pipeWith, andThen, lensProp,
-    append, apply, prop, converge, multiply, identity, modulo, concat, tryCatch
+    tap, length, test, ifElse, andThen, lensProp,
+    append, apply, prop, converge, multiply, identity, modulo, concat, tryCatch, thunkify, otherwise
 } from "ramda"
 
 import Api from '../tools/api';
@@ -26,40 +26,22 @@ import Api from '../tools/api';
 const api = new Api();
 const baseURL = 'https://api.tech/numbers/base'
 const animalURL = 'https://animals.tech/'
-const getResult = prop("result")
-const resolve = (value) => Promise.resolve(value)
 
+
+const getResult = prop("result")
 const formAnimalUrl = concat(animalURL)
 const askServerForAnimal = api.get(__, null)
 
-const getAnimal = pipeWith(andThen, [
-    resolve,
-    String,
-    formAnimalUrl,
-    askServerForAnimal,
-    getResult
-])
 
-const square = converge(
-    multiply,
-    [identity, identity]
-)
+const moduloDivBy3 = modulo(__, 3)
+const square = converge( multiply, [identity, identity])
 
 const queryParams =  {from: 10,
                         to: 2,
                     number: null}
-
 const prepareQueryParams = set(lensProp("number"), __, queryParams)
 const formValidBaseUrl = append(__, [baseURL])
 const askServerForBase = apply(api.get)
-
-const convertToBinary = pipeWith(andThen, [
-    resolve,
-    prepareQueryParams,
-    formValidBaseUrl,
-    askServerForBase,
-    getResult
-])
 
 const roundValue = compose(
     Math.round,
@@ -77,10 +59,10 @@ const validateLength =  compose(
 
 const isPositive =  compose(
     gt(__, 0),
-    roundValue
+    Number
 )
 
-const isNumber = test(new RegExp('[+-]?([0-9]*[.])?[0-9]+'))
+const isNumber = test(new RegExp('^\\d+(.\\d+)?$'))
 
 const validate = allPass([
         validateLength,
@@ -94,40 +76,71 @@ const VALIDATION_FAILURE_MESSAGE = "ValidationError"
 const processSequence = async ({
        value, writeLog, handleSuccess, handleError
 }) => {
+
+const log = tap(writeLog)
+const onError = thunkify(handleError)
+const resultFromServer = pipe(
+    andThen(getResult),
+    otherwise(handleError)
+)
+
 const prepareNumber = pipe(
     String,
     length,
-    tap(writeLog),
+    log,
     square,
-    tap(writeLog),
-    modulo(__, 3),
-
+    log,
+    moduloDivBy3,
 )
 
- const onTrue = pipeWith( andThen, [
-     resolve,
-     roundValue,
-     tap(writeLog),
-     convertToBinary,
-     tap(writeLog),
-     prepareNumber,
-     tap(writeLog),
-     getAnimal,
-     handleSuccess
- ])
+const convertToBinary = pipe(
+    String,
+    prepareQueryParams,
+    formValidBaseUrl,
+    askServerForBase,
+    andThen(getResult),
+    resultFromServer
+)
 
-const onFalse = () => { handleError(VALIDATION_FAILURE_MESSAGE) }
+const getAnimal = pipe(
+    String,
+    log,
+    formAnimalUrl,
+    askServerForAnimal,
+    resultFromServer
+)
 
-const mainProcessor =  await pipe(
-                    tap(writeLog),
+const step1 = pipe(
+    roundValue,
+    log,
+    convertToBinary,
+)
+
+const step2 = pipe(
+    log,
+    prepareNumber,
+    log,
+    getAnimal,
+    andThen(handleSuccess),
+    otherwise(handleError)
+)
+
+ const onTrue = pipe(
+     step1,
+     andThen(step2),
+     otherwise(handleError)
+ )
+
+const mainProcessor = await pipe(
+                    log,
                     ifElse(
                         validate,
                         onTrue,
-                        onFalse
+                        onError(VALIDATION_FAILURE_MESSAGE)
                     )
         )
 
-tryCatch(mainProcessor, handleError)(value)
+tryCatch(mainProcessor, onError)(value)
 
 }
 
